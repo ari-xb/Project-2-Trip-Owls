440f8d08c1f373813d879c6ec1214052fd35b6bb
ari-xb <rexb83@gmail.com>
2016-11-04 14:21:25 +1100

    forms

--- main.rb
+++ main.rb
@@ -1,6 +1,6 @@
 require 'sinatra'
 require 'pry'
-#require 'sinatra/reloader' # remove when deploying
+require 'sinatra/reloader' # remove when deploying
 require_relative 'db_config'
 require_relative 'models/users'
 require_relative 'models/trips'
